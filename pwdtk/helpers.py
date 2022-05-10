c89734e6f8105a44a802fd9a107b3bcaa00a6734
root on DESKTOP-QVMHD9H <root@DESKTOP-QVMHD9H.mhcomm.fr>
2022-05-10 14:31:58 +0200

    replace watcher by AUTH_PASSWORD_VALIDATORS -> password_changed

--- pwdtk/helpers.py
+++ pwdtk/helpers.py
@@ -2,24 +2,9 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 import importlib
-import inspect
 import six
 
 from django.conf import settings
-
-
-def recursion_depth():
-    """return recursion depth. 0 if no recursion"""
-    # taken from https://github.com/looking-for-a-job/recursion-detect.py
-    counter = 0
-    frames = inspect.getouterframes(inspect.currentframe())[1:]
-    top_frame = inspect.getframeinfo(frames[0][0])
-    for frame, _, _, _, _, _ in frames[1:]:
-        (path, line_number, func_name, lines, index) = inspect.getframeinfo(
-            frame)
-        if path == top_frame[0] and func_name == top_frame[2]:
-            counter += 1
-    return counter
 
 
 def _resolve_object_path(dotted_name):
