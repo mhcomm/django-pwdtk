6a5e16fcf56b51e972ffc0e46e13ee0e7bf39b72
kf@quenpc <kf@quenpc>
2022-05-13 14:31:13 +0200

    change requirements version limits

--- setup.py
+++ setup.py
@@ -48,6 +48,8 @@ setup(
       "Funding": "https://donate.pypi.org",
       "Tracker": "https://github.com/mhcomm/django-pwdtk/issues",
     },
+    # TODO in the newar future
+    # python_requires=">=3.6, <4",
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4",
     include_package_data=True,
     )
