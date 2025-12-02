84d21dd67a4d82ca1c69f1477dd270a31fc58bd4
Klaus Foerster <klaus.foerster@mhcomm.fr>
2025-12-02 10:00:21 +0100

    add __version__ and meta.version_info

--- pwdtk/__init__.py
+++ pwdtk/__init__.py
@@ -1,1 +1,7 @@
 default_app_config = 'pwdtk.apps.PwdTkConfig'
+
+try:
+    from importlib.metadata import version
+    __version__ = version("pwdtk")
+except ImportError:
+    from pwdtk._version import __version__  # noqa: F401
