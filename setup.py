8dabcbb577ce9e5c0133797eb85d55e6a11127f8
Jonathan Lajus <jonathan.lajus@mhcomm.fr>
2026-03-18 10:21:26 +0100

    Version 2.0.4

--- setup.py
+++ setup.py
@@ -19,12 +19,12 @@ It spans a rather wide range of python and django versions and supports:
 
 setup(
     name="django-pwdtk",
-    version="2.0.3",
+    version="2.0.4",
     description="package to tune django password authentification",
     # long_description=long_description,
     # long_description_content_type="text/x-rst",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production/Stable",
         ],
     keywords="django authentification password",
     url="https://github.com/mhcomm/django-pwdtk",
