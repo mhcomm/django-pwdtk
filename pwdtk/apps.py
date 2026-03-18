f7108b04d7b014d8e3acc60db780055b496cb58b
Jonathan Lajus <lajus@crans.org>
2026-03-18 10:14:20 +0100

    Remove usage of PwdtkSettings in django init (#69)
    * Remove usage of PwdtkSettings in django init
    
    * flake
    
    * MR comment: add comment
    
    ---------
    
    Co-authored-by: Jonathan Lajus <jonathan.lajus@mhcomm.fr>

--- pwdtk/apps.py
+++ pwdtk/apps.py
@@ -1,12 +1,10 @@
 import logging
 
 from django.apps import AppConfig
-from pwdtk.helpers import PwdtkSettings
 
 logger = logging.getLogger(__name__)
 
-
-logger.debug("imp pwdtk APPS")  # added trace for dbg of dj 1.8 -> dj 1.11
+logger.debug("imp pwdtk APPS")
 
 
 class PwdTkConfig(AppConfig):
@@ -17,6 +15,3 @@ class PwdTkConfig(AppConfig):
             The required hooks depend on the django version.
         """
         logger.debug("PWDTK READY")
-        if not PwdtkSettings.PWDTK_ENABLED:
-            logger.debug("PWDTK DISABLED")
-            return
--- pwdtk/middlewares.py
+++ pwdtk/middlewares.py
@@ -1,8 +1,6 @@
 import json
 import logging
 
-
-from django.core.exceptions import MiddlewareNotUsed
 from django.http import HttpResponse
 
 try:
@@ -11,33 +9,26 @@ except: # noqa E722
     class MiddlewareMixin(object):
         pass
 
-
 from pwdtk.auth_backends import PwdtkForceRenewException
 from pwdtk.auth_backends import PwdtkLockedException
 from pwdtk.helpers import PwdtkSettings
-
 
 logger = logging.getLogger(__name__)
 
 
 class PwdtkMiddleware(MiddlewareMixin):
 
-    def __init__(self, get_response=None):
-
-        if not PwdtkSettings.PWDTK_ENABLED:
-            logger.debug("PWDTK middleware is disabled")
-            raise MiddlewareNotUsed("pwdtk is disabled")
-        if get_response:
-            super(PwdtkMiddleware, self).__init__(
-                get_response=get_response)
-        else:
-            super(PwdtkMiddleware, self).__init__()
-
     def process_request(self, request):
+        # Will still be called even if PwdtkSettings.PWDTK_ENABLED is False.
+        # Only use safe code here or add a conditional on
+        # PwdtkSettings.PWDTK_ENABLED when necessary to keep the logic sound.
         if hasattr(PwdtkSettings, "reset_cache"):
             PwdtkSettings.reset_cache()
 
     def process_exception(self, request, exception):
+        # Will still be called even if PwdtkSettings.PWDTK_ENABLED is False.
+        # Only use safe code here or add a conditional on
+        # PwdtkSettings.PWDTK_ENABLED when necessary to keep the logic sound.
 
         if isinstance(exception, PwdtkLockedException):
             context = exception.pwdtk_data.get_lockout_context()
